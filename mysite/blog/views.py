312100d26fa135fd2d9eeaff28f4c5bdd0d1ad7e
Nicolas_Cadet <c.nicolas334@gmail.com>
2020-01-21 17:20:30 +0100

    fixes

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -69,7 +69,7 @@ class DraftListView(LoginRequiredMixin, ListView):
 @login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    post.published_date
+    post.publish()
     return redirect('post_detail', pk=pk)
 
 
